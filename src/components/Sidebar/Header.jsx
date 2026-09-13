79ed44e82506cde69adf12039333e611aa0355bc
ともや <tangkoup@gmail.com>
2026-09-13 20:44:33 +0900

    feat: サイドバー開閉処理のアニメーションを調整

--- src/components/Sidebar/Header.jsx
+++ src/components/Sidebar/Header.jsx
@@ -5,7 +5,7 @@ import { useContext } from 'react';
 import { AppContext } from '../../context.jsx';
 
 
-const Header = ({ commonStyle }) => {
+const Header = ({ commonStyle, shouldShowContent }) => {
 
     const { setSidebarIsOpened, sidebarIsOpened, setCurrentChatId } = useContext(AppContext);
 
@@ -15,42 +15,66 @@ const Header = ({ commonStyle }) => {
          flex flex-col justify-between
          ${commonStyle.padding}`}>
 
-            <div className="flex justify-between h-6">
-                {sidebarIsOpened ? (
+            <div className={`relative h-6.25 w-full `}>
+
+                {shouldShowContent && (
                     <>
-                        <h1 className="text-[18px] cursor-pointer" onClick={() => setCurrentChatId('start')}>ReactGPT</h1>
-                        <div className="flex gap-1.25">
+                        <h1 className={`
+                            absolute left-0
+                            text-xl 
+                            cursor-pointer 
+                            ${commonStyle.transition}
+                            transition-opacity
+                            ${sidebarIsOpened ? 'opacity-100' : 'opacity-0'}`}
+                            onClick={() => setCurrentChatId('start')}>
+                            ReactGPT
+                        </h1>
+                        <button className={`
+                        flex gap-1.25 
+                        absolute right-0
+                         ${commonStyle.transition}
+                            transition-opacity
+                            ${sidebarIsOpened ? 'opacity-100' : 'opacity-0'}`}
+                            type="button" onClick={() => setSidebarIsOpened(false)}>
                             <FiSidebar
                                 size={25}
-                                onClick={() => setSidebarIsOpened(false)}
-                                className="cursor-ew-resize shrink-0"
-                            />
-                        </div>
+                                className="cursor-ew-resize shrink-0" />
+                        </button>
                     </>
-                ) : (
-                    <button onClick={() => setSidebarIsOpened(true)} className={`cursor-pointer ${commonStyle.borderRadius}`}>
-                        <FaReact size={25} className="shrink-0 cursor-ew-resize" />
-                    </button>
                 )}
+                {!shouldShowContent && (
+                    <button className='cursor-pointer  absolute left-0'
+                        type="button" onClick={() => setSidebarIsOpened(true)}>
+                        <FaReact size={25} className="shrink-0 cursor-ew-resize" />
+                    </button>)}
             </div>
+
+
+
+
 
             <button className={`
                         flex justify-start items-center 
-                        h-6 
+                        h-6.25 
                         cursor-pointer
                         ${commonStyle.borderRadius}
                         ${commonStyle.hover}`}
                 onClick={() => setCurrentChatId('start')}>
                 <HiOutlinePencilSquare size={25} className="shrink-0" />
-                <div className={`
-                    ${commonStyle.transition}
-                    transition-opacity
-                    ${sidebarIsOpened ? 'opacity-100' : 'opacity-0'}`}>
-                    新しいチャット
-                </div>
+
+                {shouldShowContent && (
+                    <div className={`
+                            shrink-0
+                            whitespace-nowrap
+                            ${commonStyle.transition}
+                            transition-opacity
+                            ${sidebarIsOpened ? 'opacity-100' : 'opacity-0'}
+                            `}>
+                        新しいチャット
+                    </div>)}
             </button>
 
-        </header>
+        </header >
     );
 };
 
