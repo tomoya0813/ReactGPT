ced0249a4f1eddb3dd4361c795d21c1dc56e227e
ともや <tangkoup@gmail.com>
2026-09-14 01:56:59 +0900

    feat: チャット最後のメッセージに自動スクローする機能を追加

--- src/components/MainArea/TextDisplay.jsx
+++ src/components/MainArea/TextDisplay.jsx
@@ -1,13 +1,21 @@
 import { FaGithub } from "react-icons/fa";
 import { IoMailSharp } from "react-icons/io5";
 import { FaRegMoon } from "react-icons/fa";
-import { useContext } from 'react';
+import { useContext, useRef, useEffect } from 'react';
 import { AppContext } from '../../context.jsx';
 
 const TextDisplay = ({ commonStyle }) => {
     const { history, currentChatId } = useContext(AppContext);
+    const ref = useRef(null)
 
-    const currentChat = history.find((index) => index.id === currentChatId);
+
+    const currentChat = history.find((chat) => chat.id === currentChatId);
+    const currentLastMessageIndex = currentChat ?
+        currentChat.messages.length - 1 : null;
+
+    useEffect(() => {
+        ref.current?.scrollIntoView();
+    }, [currentChatId, currentLastMessageIndex])
 
     return (
         <div className='
@@ -15,49 +23,48 @@ const TextDisplay = ({ commonStyle }) => {
             w-full
             flex flex-col items-center gap-12
             '>
-            {
-
-                currentChat && currentChat.messages.map((e, index) => (
-                    <div key={index}
-                        className={`
+            {currentChat && currentChat.messages.map((e, index) => (
+                <div key={index}
+                    ref={index === currentLastMessageIndex ? ref : null}
+                    className={`
                             flex flex-col gap-12
                             ${commonStyle.width}`}>
-                        <div className='
+                    <div className='
                             self-end
                             bg-blue-200
                             pr-7 pl-7 pt-5 pb-5
                             min-w-15 max-w-full
                             rounded-3xl'>{e}</div>
-                        <div className='self-start 
+                    <div className='self-start 
                                   bg-gray-100 
                                     min-w-15 max-w-full
                                     pr-7 pl-7 pt-5 pb-5
                                     rounded-3xl'>
-                            <p className='mb-3'>返信機能は実装していません</p>
-                            <p className='mb-1.5'>各種リンクはこちら↓</p>
-                            <ul className='flex flex-col gap-2'>
-                                <li className="flex items-center gap-2">
-                                    <FaGithub />
-                                    <a href='https://github.com/tomoya0813' className='text-blue-500 underline hover:text-blue-700'>
-                                        GitHub
-                                    </a>
-                                </li>
-                                <li className="flex items-center gap-2">
-                                    <IoMailSharp />
-                                    <a href='https://tomoya0813.github.io/portfolio/contact' className='text-blue-500 underline hover:text-blue-700'>
-                                        Email
-                                    </a>
-                                </li>
-                                <li className="flex items-center gap-2">
-                                    <FaRegMoon />
-                                    <a href='https://tomoya0813.github.io/portfolio/about/index.html' className='text-blue-500 underline hover:text-blue-700'>
-                                        MY About
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
+                        <p className='mb-3'>返信機能は実装していません</p>
+                        <p className='mb-1.5'>各種リンクはこちら↓</p>
+                        <ul className='flex flex-col gap-2'>
+                            <li className="flex items-center gap-2">
+                                <FaGithub />
+                                <a href='https://github.com/tomoya0813' className='text-blue-500 underline hover:text-blue-700'>
+                                    GitHub
+                                </a>
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <IoMailSharp />
+                                <a href='https://tomoya0813.github.io/portfolio/contact' className='text-blue-500 underline hover:text-blue-700'>
+                                    Email
+                                </a>
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <FaRegMoon />
+                                <a href='https://tomoya0813.github.io/portfolio/about/index.html' className='text-blue-500 underline hover:text-blue-700'>
+                                    MY About
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                ))}
+                </div>
+            ))}
         </div>
     )
 }
