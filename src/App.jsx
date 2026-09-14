d717df1e405752b5182e53840566f63ac2ba3705
ともや <tangkoup@gmail.com>
2026-09-14 23:53:18 +0900

    feat: モバイル時の挙動制御

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./components/Sidebar/Sidebar"
 import MainArea from "./components/MainArea/MainArea"
 
 function App() {
-  const { sidebarIsOpened } = useContext(AppContext)
+  const { sidebarIsOpened, isMobile } = useContext(AppContext)
 
   const layoutState = [
     {
@@ -28,22 +28,26 @@ function App() {
   // console.log(sidebarTransition)
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen ">
       <div
         style={{
           width: `${currentLayout.sidebarWidth}px`,
-          transitionDuration: `${sidebarTransitionTime}ms`
+          transitionDuration: `${sidebarTransitionTime}ms`,
+          position: isMobile ? 'absolute' : 'static',
+          transform: `translateX(${isMobile && !sidebarIsOpened ? '-100%' : '0'})`,
+          zIndex: isMobile ? 2 : 0,
+          background: isMobile ? 'white' : 'transparent'
         }}
         className={`
         h-screen
         border-r 
         border-[rgba(0,0,0,0.05)] 
-         transition-[width]
+         transition-[width,transform]
         `}
       >
         <Sidebar sidebarTransition={sidebarTransition} sidebarTransitionTime={sidebarTransitionTime} />
       </div>
-      <div className={`h-screen flex-1 `}>
+      <div className={`h-screen flex-1 ${isMobile && sidebarIsOpened ? 'blur-xs' : ''}`}>
         <MainArea layoutState={layoutState} />
       </div>
     </div >
