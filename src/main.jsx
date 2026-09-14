b0ff244a094d07b1c836eb3636864c36411094c6
ともや <tangkoup@gmail.com>
2026-09-14 23:56:31 +0900

    refactor: 不要なフラグメント削除

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <StrictMode>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </StrictMode>
-  </>
+  <StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </StrictMode>
 )
