2a923c99fe20a5a1a4298d8429fdc98ad59e316f
ともや <tangkoup@gmail.com>
2026-09-14 19:15:21 +0900

    feat: 設定用オブジェクト追加、スクロールバーのgutter幅を取得してレイアウトに反映

--- src/components/MainArea/MainArea.jsx
+++ src/components/MainArea/MainArea.jsx
@@ -1,16 +1,51 @@
-import { useContext } from 'react';
+import { useContext, useRef, useEffect, useState } from 'react';
 import { AppContext } from '../../context.jsx';
 import UserActions from './UserActions';
 import TextDisplay from './TextDisplay';
 import { v4 as uuidv4 } from 'uuid';
+import { BiAlignLeft } from "react-icons/bi";
 
 const MainArea = ({ layoutState }) => {
 
-  const { history, setHistory,
+
+  // =====================================================================
+
+  const config = {
+    padding: 20,
+    contentsWidth: 'lg:w-175 md:w-130 w-full'
+  }
+
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  const {
+    history, setHistory,
     currentChatId, setCurrentChatId,
-    texts, setCurrentTexts, sidebarIsOpened, windowWidth } = useContext(AppContext);
+    texts, setCurrentTexts, sidebarIsOpened, setSidebarIsOpened, windowWidth, isMobile
+  } = useContext(AppContext);
+
+  const scrollRef = useRef(null);
+  const [gutterWidth, setGutterWidth] = useState(0);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      setGutterWidth(scrollRef.current.offsetWidth - scrollRef.current.clientWidth)
+      // console.log('gutter:', gutterWidth);
+    }
+  }, [currentChatId]);
+
+
+  const commonStyle = {
+    displayPadding: config.padding,
+    UserActionsPadding: gutterWidth / 2 + config.padding
+  }
 
   const userActionsPosition = () => {
+    if (isMobile) {
+      return {
+        left: 0,
+      }
+    }
     const sideW = layoutState.find(state => state.sidebarIsOpened === sidebarIsOpened)
       .sidebarWidth;
     const mainW = windowWidth - sideW;
@@ -19,55 +54,87 @@ const MainArea = ({ layoutState }) => {
       left, transform: 'translateX(-50%)'
     }
   }
+
+
   const handleSubmit = () => {
-    if (texts[currentChatId].trim() === '') return;
+
+    const currentText = texts[currentChatId] || '';
+    if (currentText.trim() === '') return;
 
     if (currentChatId === 'start') {
       const temp = uuidv4();
-      setHistory([...history, { id: temp, messages: [texts[currentChatId]] }])
+      setHistory([
+        ...history,
+        { id: temp, messages: [currentText] }
+      ])
       setCurrentChatId(temp)
     } else {
       setHistory(history.map((item) => {
         if (item.id === currentChatId) {
-          return { ...item, messages: [...item.messages, texts[currentChatId]] }
+          return {
+            ...item,
+            messages: [...item.messages, currentText]
+          }
         }
         return item;
       }))
     }
+
     setCurrentTexts('')
   }
 
-  const commonStyle = {
-    width: 'lg:w-175 md:w-130'
-  }
 
   return (
     <div className="h-screen">
       {currentChatId === 'start' ? (
-        <div className={`fixed bottom-1/2 `} style={userActionsPosition()}>
-          <UserActions
-            text={texts[currentChatId] || ''}
-            setCurrentTexts={setCurrentTexts}
-            handleSubmit={handleSubmit}
-            commonStyle={commonStyle}
-          />
-        </div>
-      ) : (
         <>
-          <div className="h-screen overflow-y-auto  scrollbar-gutter-both">
-            <TextDisplay commonStyle={commonStyle} />
-          </div>
-          <div className={`fixed bottom-7`} style={userActionsPosition()}>
+          {isMobile &&
+            <button className='fixed top-2.5 left-2.5 cursor-pointer bg-white p-1 rounded-xs z-1'
+              onClick={() => setSidebarIsOpened(true)}>
+              <BiAlignLeft size={25} />
+            </button>}
+
+          <div className={`fixed bottom-1/2 ${config.contentsWidth}`}
+            style={{
+              ...userActionsPosition(),
+              paddingLeft: `${commonStyle.UserActionsPadding}px`,
+              paddingRight: `${commonStyle.UserActionsPadding}px`,
+            }}>
             <UserActions
               text={texts[currentChatId] || ''}
               setCurrentTexts={setCurrentTexts}
               handleSubmit={handleSubmit}
-              commonStyle={commonStyle}
             />
           </div>
         </>
-      )}
-    </div>
+      ) : (
+        <>
+          {isMobile &&
+            <button className='fixed top-2.5 left-2.5 cursor-pointer bg-white p-1 rounded-xs z-1'
+              onClick={() => setSidebarIsOpened(true)}>
+              <BiAlignLeft size={25} />
+            </button>}
+          <div ref={scrollRef}
+            className="h-screen overflow-y-auto scrollbar-gutter-both  w-full"
+            style={{ padding: `${commonStyle.displayPadding}px` }} >
+            <TextDisplay />
+          </div>
+          <div className={`fixed bottom-7 ${config.contentsWidth}`}
+            style={{
+              ...userActionsPosition(),
+              paddingLeft: `${commonStyle.UserActionsPadding}px`,
+              paddingRight: `${commonStyle.UserActionsPadding}px`,
+            }}>
+            <UserActions
+              text={texts[currentChatId] || ''}
+              setCurrentTexts={setCurrentTexts}
+              handleSubmit={handleSubmit}
+            />
+          </div>
+        </>
+      )
+      }
+    </div >
   )
 }
 
